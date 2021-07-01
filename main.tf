7f5edda4bdcb869be2f5cdc841a4e14330e26257
root <root@jenkins.local>
2021-07-01 18:53:12 +0200

    ssh-key

--- main.tf
+++ main.tf
@@ -11,6 +11,11 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
+resource "aws_key_pair" "example" {
+  key_name   = "examplekey"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
 variable "instance_count" {
   default = "2"
 }
@@ -25,15 +30,25 @@ variable "aws_region" {
 
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  #region  = "us-east-1"
+  region = var.aws_region
 }
 
-resource "aws_instance" "Instance_mysql" {
+resource "aws_instance" "Instance" {
+  key_name      = aws_key_pair.example.key_name
   count         = var.instance_count
   ami		 = "ami-0b0af3577fe5e3532"
   #instance_type = "t2.micro"
   instance_type = var.instance_type
-
+  
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/terraform")
+    host        = self.public_ip
+  }
+  
+  #provisioner "remote-exec"
 
   tags = {
     Name  = "Instance${count.index + 1}"
