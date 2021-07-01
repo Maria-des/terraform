5db550374671dcac2a1343e41ef4319b6b0be34e
root <root@jenkins.local>
2021-07-01 15:09:59 +0200

    2 instances

--- main.tf
+++ main.tf
@@ -23,16 +23,21 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
 resource "aws_instance" "Instance_mysql" {
-  #count         = var.instance_count
+  count         = var.instance_count
   ami		 = "ami-0b0af3577fe5e3532"
   #instance_type = "t2.micro"
   instance_type = var.instance_type
 
 
   tags = {
-    #Name  = "Instance${count.index + 1}"
-    Name = "InstanceMySql"
+    Name  = "Instance${count.index + 1}"
+    #Name = "InstanceMySql"
   }
 }
 
