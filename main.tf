6f43fcd028f8e357e32a5f324ff0f9886769432f
root <root@jenkins.local>
2021-07-03 12:27:15 +0200

    ansible install

--- main.tf
+++ main.tf
@@ -34,30 +34,33 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "Instance" {
+resource "aws_instance" "InstanceMysql" {
   key_name      = aws_key_pair.example.key_name
   #count         = var.instance_count
   ami		 = "ami-0b0af3577fe5e3532"
   #instance_type = "t2.micro"
   instance_type = var.instance_type
   
-  #connection {
-  #  type        = "ssh"
-  #  user        = "ec2-user"
-  #  private_key = file("~/.ssh/terraform")
-  #  #host        = self.public_ip
-  #}
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
+  }
   
   provisioner "remote-exec" {
     #connection {
     #type        = "ssh"
     #user        = "ec2-user"
     #private_key = file("~/.ssh/terraform")
-    #host        = "${element(aws_instance.Instance[0].public_ip, count.index)}"
+    #host        = "${element(aws_instance.Instance*.public_ip, count.index)}"
+    #host        = self.public_ip
     inline = [
-        "sudo amazon-linux-extras enable tomcat",
-        "sudo yum -y install tomcat"
-    #  #"sudo systemctl start nginx"
+        "sudo hostnamectl set-hostname mysql",
+        "sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm",
+	"sudo dnf config-manager --set-enabled codeready-builder-for-rhel-8-rhui-rpms",
+	"sudo dnf install -y ansible",
+	"ansible --version"
     ]
   } 
 
--- tomcat.tf
+++ tomcat.tf
@@ -7,18 +7,20 @@ resource "aws_instance" "InstanceTomcat" {
   #instance_type = "t2.micro"
   instance_type = var.instance_type
   
-  #connection {
-  #  type        = "ssh"
-  #  user        = "ec2-user"
-  #  private_key = file("~/.ssh/terraform")
-  #  #host        = self.public_ip
-  #}
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
+  }
   
   provisioner "remote-exec" {
     inline = [
-        "sudo amazon-linux-extras enable tomcat",
-        "sudo yum -y install tomcat"
-    #  #"sudo systemctl start nginx"
+      "sudo hostnamectl set-hostname tomcat",
+      "sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm",
+      "sudo dnf config-manager --set-enabled codeready-builder-for-rhel-8-rhui-rpms",
+      "sudo dnf install -y ansible",
+      "ansible --version"
     ]
   } 
 
